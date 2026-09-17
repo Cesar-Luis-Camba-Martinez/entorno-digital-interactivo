8e5691a9ecdb760a02ad2798465f1d7d4cc209ab
César Luis Camba Martínez <cesar.cambamar@ug.edu.ec>
2026-09-17 14:05:09 -0500

    Simplify equation steps in script.js

--- script.js
+++ script.js
@@ -796,12 +796,10 @@ function resolverFraccionaria() {
       <br><em>Definición:</em> Si el cálculo final diera $x = ${restriccion}$, no habría solución válida.
     </div>
     <div><strong>Paso 2: Transposición del término independiente $c$:</strong>
-      <br>Escribimos la ecuación y pasamos el término $c = ${c}$ restando al miembro derecho:
-      <br>$$\\frac{${a}}{x ${b >= 0 ? '+ ' + b : '- ' + Math.abs(b)}} = ${-c}$$
+      <br>Escribimos la ecuación y pasamos el término $c = ${c}$ restando al miembro derecho: $\\frac{${a}}{x ${b >= 0 ? '+ ' + b : '- ' + Math.abs(b)}} = ${-c}$
     </div>
     <div><strong>Paso 3: Multiplicación por el denominador para despejar la fracción:</strong>
-      <br>Multiplicamos ambos lados de la ecuación por el binomio $(x ${b >= 0 ? '+ ' + b : '- ' + Math.abs(b)})$:
-      <br>$${a} = ${-c} \\cdot (x ${b >= 0 ? '+ ' + b : '- ' + Math.abs(b)})$
+      <br>Multiplicamos ambos lados de la ecuación por el binomio $(x ${b >= 0 ? '+ ' + b : '- ' + Math.abs(b)})$: $${a} = ${-c} \\cdot (x ${b >= 0 ? '+ ' + b : '- ' + Math.abs(b)})$
     </div>
     <div><strong>Paso 4: Despeje del binomio $(x + b)$:</strong>
       <br>Pasamos ${-c} a dividir al lado izquierdo:
